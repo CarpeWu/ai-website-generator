309be1f0d1a70a8525125b3901ce17c0ef210a69
CarpeWu <agtg@foxmail.com>
2025-08-30 21:24:08 +0800

    教程第 10 期 - AI 工作流（LangGraph4j 实现网站生成工作流） 1. 依次编写简单工作流图、有状态工作流图、模拟状态流转工作流图，渐进式开发 2. 开发图片收集工具和 AI 服务 3. 开发各个工作节点 4. 开发实际应用的代码生成工作流

--- src/main/java/com/carpe/aicodemother/config/ReasoningStreamingChatModelConfig.java
+++ src/main/java/com/carpe/aicodemother/config/ReasoningStreamingChatModelConfig.java
@@ -21,14 +21,20 @@ public class ReasoningStreamingChatModelConfig {
      */
     @Bean
     public StreamingChatModel reasoningStreamingChatModel() {
+
+        // 通义
 //        // 为了测试方便临时修改
-//        final String modelName = "qwen3-1.7b";
+//        final String baseUrl = "https://dashscope.aliyuncs.com/compatible-mode/v1";
+//        final String modelName = "qwen-plus-2025-04-28";
 //        final int maxTokens = 8192;
         // 生产环境使用：
 //         final String modelName = "qwen3-235b-a22b-thinking-2507";
 //         final int maxTokens = 32768;
 
+
+        final String baseUrl = "https://api.deepseek.com";
         final String modelName = "deepseek-chat";
+        final String apiKey = "YOUR_DEEPSEEK_API_KEY";
         final int maxTokens = 8192;
         // 生产环境使用
         // final String modelName = "deepseek-reasoner";
