f6be659b3e073a38961f0f4c72506829dc834ae3
CarpeWu <agtg@foxmail.com>
2025-09-01 11:35:07 +0800

    教程第 11 期 - 系统优化（实时性优化） 1. Vue 工程异步构建改为同步构建，构建完成后才结束 SSE 连接

--- src/main/java/com/carpe/aicodemother/core/handler/JsonMessageStreamHandler.java
+++ src/main/java/com/carpe/aicodemother/core/handler/JsonMessageStreamHandler.java
@@ -6,7 +6,6 @@ import cn.hutool.json.JSONUtil;
 
 import com.carpe.aicodemother.ai.model.message.*;
 import com.carpe.aicodemother.ai.tools.BaseTool;
-import com.carpe.aicodemother.constant.AppConstant;
 import com.carpe.aicodemother.core.builder.VueProjectBuilder;
 import com.carpe.aicodemother.model.entity.User;
 import com.carpe.aicodemother.model.enums.ChatHistoryMessageTypeEnum;
@@ -27,9 +26,6 @@ import java.util.Set;
 @Slf4j
 @Component
 public class JsonMessageStreamHandler {
-
-    @Resource
-    private VueProjectBuilder vueProjectBuilder;
 
     @Resource
     private ToolManager toolManager;
@@ -61,9 +57,6 @@ public class JsonMessageStreamHandler {
                     // 流式响应完成后，添加 AI 消息到对话历史
                     String aiResponse = chatHistoryStringBuilder.toString();
                     chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
-                    // 异步构造 Vue 项目
-                    String projectPath = AppConstant.CODE_OUTPUT_ROOT_DIR + "/vue_project_" + appId;
-                    vueProjectBuilder.buildProjectAsync(projectPath);
                 })
                 .doOnError(error -> {
                     // 如果AI回复失败，也要记录错误消息
